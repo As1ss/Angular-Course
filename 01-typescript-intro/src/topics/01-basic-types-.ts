8d39ed3662b1a489562157e11ce2f2884fe57de1
As1ss <alexis.lopbri@educa.jcyl.es>
2024-03-13 09:11:29 +0100

    Realizado ejemplos de tipos basicos

---
+++ 01-typescript-intro/src/topics/01-basic-types-.ts
@@ -0,0 +1,18 @@
+
+
+const name:string = "Strider";
+let healthPoints:number | string = 95;
+const isAlive: boolean = true;
+
+
+healthPoints = "FULL";
+
+console.log({
+    name,healthPoints,isAlive
+})
+
+
+
+
+
+export{}; 
