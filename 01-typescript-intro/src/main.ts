837a747d98b92f76d5b91ddaab57e1d7b7f69e85
As1ss <alexis.lopbri@educa.jcyl.es>
2024-03-13 12:10:40 +0100

    Desestructuración de objetos

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,7 +2,9 @@ import './style.css'
 // import "./topics/01-basic-types"
 // import "./topics/02-object-interface"
 // import "./topics/03-functions"
-import "./homework/01-homework-types"
+// import "./homework/01-homework-types"
+
+import "./topics/04-basic-destructuring"
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
