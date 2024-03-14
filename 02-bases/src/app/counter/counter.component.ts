9597abbf18d63979d87e9d2b48a0941b8da1a691
As1ss <alexis.lopbri@educa.jcyl.es>
2024-03-14 13:49:53 +0100

    Ejemplo creación de un componente realizado, contador component

---
+++ 02-bases/src/app/counter/counter.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: "<h1>Hola Counter</h1>"
+})
+
+export class CounterComponent{
+  constructor() { }
+}
