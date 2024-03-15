81505dbfe6b447ded2d599a0cb7817d52a7dd65a
As1ss <alexis.lopbri@educa.jcyl.es>
2024-03-15 13:03:16 +0100

    Realizado más ejemplos de directivas con ngIf, else y el uso del elemento <ng-template>

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -10,21 +10,25 @@ import { lastValueFrom } from 'rxjs';
 export class ListComponent {
 
   public heroNames:string[]=["Spiderman","Ironman","Hulk","Thor","DrStrange"];
+  public heroDeleted?:string;
 
-  public deleteLastHeroName(){
+  public deleteLastHeroName():void{
 
 
-    let listLength= this.heroNames.length-1;
+    // let listLength= this.heroNames.length-1;
+    // if(listLength<0){
+    //     alert("Cant delete more Heroes");
+    // }
+    // else{
+    //   alert(`Hero deleted: ${this.heroNames[listLength]}`);
+    //   delete this.heroNames[listLength];
+    //   this.heroNames.length=listLength;
+    //   console.log(this.heroNames.length);
+    // }
 
-    if(listLength<0){
-        alert("Cant delete more Heroes");
-    }
-    else{
-      alert(`Hero deleted: ${this.heroNames[listLength]}`);
-      delete this.heroNames[listLength];
-      this.heroNames.length=listLength;
-      console.log(this.heroNames.length);
-    }
+    //this.heroNames.pop();
+
+     this.heroDeleted = this.heroNames.pop();
 
 
 
