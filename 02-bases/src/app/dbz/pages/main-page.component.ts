78871d1bcf5d19a08f0f7bcb1f554be1b9d60236
As1ss <alexis.lopbri@educa.jcyl.es>
2024-03-18 12:21:29 +0100

    Realizada lógica de añadido de elementos al listado de personajes de dbz con anotaciones como @Output e @Input para mostrar y compartir la información entre componentes padre e hijo

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,12 +1,36 @@
 import { Component } from "@angular/core";
+import { Character } from "../interfaces/character.interface";
 
 
 
 
 @Component({
-  selector:"app-dbz-main-page",
-  templateUrl:"./main-page.component.html"
+  selector: "dbz-main-page",
+  templateUrl: "./main-page.component.html"
 })
-export class MainPageComponent{
+export class MainPageComponent {
+
+  public receivedCharacter:Character={
+    name:"",
+    power:0
+  };
+
+  public characters: Character[] = [{
+    name: "Krillin",
+    power: 1000
+  },
+  {
+    name: "Goku",
+    power: 9500
+  },
+{
+  name: "Vegeta",
+  power: 7500
+}];
+
+
+public addCharacter(character:Character):void{
+    this.characters.push(character);
+}
 
 }
