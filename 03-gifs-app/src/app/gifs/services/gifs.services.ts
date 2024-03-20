4d7c8931399bc33e84b381cb7616969f37f4d774
As1ss <alexis.lopbri@educa.jcyl.es>
2024-03-20 16:34:39 +0100

    Realizada lógica de historial de tags

--- 03-gifs-app/src/app/gifs/services/gifs.services.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.services.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 
 
+
 @Injectable({
   providedIn: "root"
 }) export class GifsService {
@@ -15,9 +16,26 @@ import { Injectable } from "@angular/core";
 
   }
 
-  public searchTag(tag:string):void{
 
-    this._tagsHistory.unshift(tag);
+
+  public searchTag(tag: string): void {
+
+    const finalTag = tag.toLocaleLowerCase();
+
+    if (finalTag.length == 0) return;
+
+    if (this._tagsHistory.includes(finalTag)) {
+
+      this._tagsHistory.splice(this._tagsHistory.indexOf(finalTag), 1);
+
+      this._tagsHistory.unshift(finalTag);
+
+    }
+    else {
+      this._tagsHistory.unshift(finalTag);
+      this._tagsHistory.splice(10);
+
+    }
 
 
   }
