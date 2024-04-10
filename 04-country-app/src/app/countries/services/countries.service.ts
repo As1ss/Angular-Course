3e10c0d69cf3a617c9cb1b2e072498fef59ba052
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-10 10:50:55 +0200

    Proyecto CountryApp realizada tarea de implementacion de búsqueda de información de países por País y por Región

--- 04-country-app/src/app/countries/services/countries.service.ts
+++ 04-country-app/src/app/countries/services/countries.service.ts
@@ -6,19 +6,37 @@ import { Country } from "../interfaces/country";
 
 
 @Injectable({
-  providedIn:"root"
-})export class CountriesService{
+  providedIn: "root"
+}) export class CountriesService {
 
-  apiURL:string = "https://restcountries.com/v3.1";
+  apiURL: string = "https://restcountries.com/v3.1";
 
-  constructor(private httpClient:HttpClient){}
+  constructor(private httpClient: HttpClient) { }
 
 
-  searchCapital(term:string): Observable<Country[]>{
+  searchCapital(term: string): Observable<Country[]> {
 
-    const url= `${this.apiURL}/capital/${term}`;
+    const url = `${this.apiURL}/capital/${term}`;
 
     return this.httpClient.get<Country[]>(url)
-    .pipe(catchError(error=> of ([]))); // Este pipe captura el error si salta y devuelve un Array vacio de Observables
+      .pipe(catchError(error => of([]))); // Este pipe captura el error si salta y devuelve un Array vacio de Observables
   }
+
+  searchCountry(term: String): Observable<Country[]> {
+    const url: string = `${this.apiURL}/name/${term}`;
+
+    return this.httpClient.get<Country[]>(url)
+      .pipe(
+        catchError(erro => of([]))
+      );
+  }
+
+  searchRegion(term: string): Observable<Country[]> {
+
+    const url: string = `${this.apiURL}/region/${term}`;
+
+    return this.httpClient.get<Country[]>(url)
+      .pipe(catchError(error => of([])));
+  }
+
 }
