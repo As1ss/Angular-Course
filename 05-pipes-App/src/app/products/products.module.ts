f01a252a292f0d81ceacd5f5f753f18d8bda8a9a
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-15 09:59:50 +0200

    Proyecto PipeApp realizdo pipes de modificacion de strings, colecciones, mostrar objetos , suscripciones a observables y promesas

--- 05-pipes-App/src/app/products/products.module.ts
+++ 05-pipes-App/src/app/products/products.module.ts
@@ -5,12 +5,14 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { BasicPagesComponent } from './pages/basics-page/basic-pages.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
 
 @NgModule({
   declarations: [
     BasicPagesComponent,
-    NumbersPageComponent
+    NumbersPageComponent,
+    UncommonPageComponent
   ],
   imports: [
     CommonModule,
