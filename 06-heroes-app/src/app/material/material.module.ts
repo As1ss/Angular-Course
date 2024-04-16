06b3948bedf6f601df517049c108779f8df09a53
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-16 09:38:00 +0200

    Proyecto HeroesApp realizada estructura de pages y routes

---
+++ 06-heroes-app/src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Error404PageComponent } from '../shared/pages/error404-page/error404-page.component';
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  exports: [],
+})
+export class MaterialModule {}
