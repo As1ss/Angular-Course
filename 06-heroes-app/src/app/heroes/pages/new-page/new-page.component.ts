51d42950144bd537b94c9b47aa16430bd8059f75
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-18 10:19:42 +0200

    Proyecto HeroesApp realizado operación Create de un hero

--- 06-heroes-app/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroes-app/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,15 +1,50 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'heroes-new-page',
   templateUrl: './new-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class NewPageComponent {
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
   public publishers = [
-    {id:'DC Comics',value: "DC - Comics"},
-    {id:'Marvel Comics',value: "Marvel - Comics"},
+    { id: 'DC Comics', value: 'DC - Comics' },
+    { id: 'Marvel Comics', value: 'Marvel - Comics' },
   ];
 
+  constructor(private heroesService: HeroesService) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero).subscribe((hero) => {
+        // TODO: mostrar snackbar
+      });
+      return;
+    }
+    this.heroesService.addHero(this.currentHero).subscribe(
+      hero=>{
+        // TODO: mostrar snackbar y navegar a /heroes/edit/ hero.id
+      }
+    )
+
+    // this.heroesService.updateHero( this.heroForm.value );
+  }
 }
