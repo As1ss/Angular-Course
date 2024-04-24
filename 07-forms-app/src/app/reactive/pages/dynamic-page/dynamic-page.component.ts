c49f5a8251e818b20d94028e76adcf439d8b0f63
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-24 12:36:52 +0200

    Proyecto FormsApp realizado validadores dinamicos en dynamicPageComponent

--- 07-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,73 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class DynamicPageComponent {
+  // public myForm:FormGroup = new FormGroup({
+  //   name: new FormControl(["",Validators.required,Validators.minLength(3)]),
+  //   favoriteGames: new FormArray([
 
+  //   ])
+  // })
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  ifsValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray:FormArray, index: number) {
+    return formArray.controls[index].errors && formArray.controls[index].touched;
+
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key in errors) {
+      switch (key) {
+        case 'required':
+          return 'Valor requerido.';
+        case 'minlength':
+          return `El valor requerido debe tener mínimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+    return null;
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+  }
 }
