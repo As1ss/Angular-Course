ff802683a49636734f76fbb9250a9b5332871eaa
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-25 13:17:06 +0200

    Proyecto FormsApp realizado validadores email y nombre y apellido

--- 07-forms-app/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-forms-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as CustomValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,9 +10,9 @@ export class RegisterPageComponent {
 
 
   public myForm:FormGroup = this.fb.group({
-    name: ["",[Validators.required]],
-    email: ["",[Validators.required]],
-    userName: ["",[Validators.required]],
+    name: ["",[Validators.required, Validators.pattern(CustomValidators.firstNameAndLastnamePattern)]],
+    email: ["",[Validators.required, Validators.pattern(CustomValidators.emailPattern)]],
+    userName: ["",[Validators.required,CustomValidators.cantBeStrider]],
     password: ["",[Validators.required, Validators.minLength(6)]],
     password2: ["",[Validators.required]]
 
