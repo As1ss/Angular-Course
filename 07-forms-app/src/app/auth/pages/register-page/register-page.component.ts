ac5797460b25fce591347eaa0387a83e38f8aa16
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-25 12:32:00 +0200

    Proyecto FormsApp formulario de registro creado

--- 07-forms-app/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-forms-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
+
 })
 export class RegisterPageComponent {
 
+
+  public myForm:FormGroup = this.fb.group({
+    name: ["",[Validators.required]],
+    email: ["",[Validators.required]],
+    userName: ["",[Validators.required]],
+    password: ["",[Validators.required, Validators.minLength(6)]],
+    password2: ["",[Validators.required]]
+
+  })
+
+
+  constructor(private fb:FormBuilder){}
+
+
+  isValidField(field:string):void{
+    // TODO: obtener validación desde un servicio
+
+
+  }
+
+
+  onSave(){
+
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+
+    console.log(this.myForm.value);
+  }
 }
