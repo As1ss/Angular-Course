c188c89d0e0f87c92151bcd0a9354242e2a5fcb7
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-26 15:01:15 +0200

    Proyecto Selectores busqueda borders bugged, no se porque no los obtiene

--- 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries-service.service';
 import { Region, SmallCountry } from '../../interfaces/country.interface';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -13,7 +13,7 @@ export class SelectorPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
   });
 
   private _countries: SmallCountry[] = [];
@@ -29,6 +29,7 @@ export class SelectorPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.onRegionChanged();
+    this.onCountryChanged();
   }
 
   // onRegionChanged():void {
@@ -43,11 +44,23 @@ export class SelectorPageComponent implements OnInit {
     this.myForm
       .get('region')!
       .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
         switchMap((region) =>
           this.countriesService.getCountriesByRegion(region)
         )
       )
       .subscribe((countries) => (this._countries = countries));
+  }
+
+  onCountryChanged(): void {
+    this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        // switchMap((region) =>
+        //   this.countriesService.getCountriesByRegion(region))
+      )
+      .subscribe(borders => console.log({ borders }));
   }
 
   get countries() {
