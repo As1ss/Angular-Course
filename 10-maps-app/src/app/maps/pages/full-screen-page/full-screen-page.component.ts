9f161de4d0643fa1e518a98e873c89891720ff32
As1ss <alexis.lopbri@educa.jcyl.es>
2024-04-30 13:51:55 +0200

    Proyecto MapsApp realizado refactorizacion del elemento html ViewChildren()

--- 10-maps-app/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ 10-maps-app/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
-import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
 
 
 
@@ -10,11 +11,17 @@ import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-g
 })
 export class FullScreenPageComponent implements AfterViewInit {
 
+  @ViewChild("map")
+  public divMap?:ElementRef;
+
 
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
+
+    if(!this.divMap) return;
+
+    const map = new Map({
       accessToken:"pk.eyJ1IjoiYXMxc3MiLCJhIjoiY2x2bTU0bzFuMDMxZTJxczM2bXhjd2dzMSJ9.xYq12_fIHO6NrKAl8VLZ5g",
-      container: 'map', // container ID
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
