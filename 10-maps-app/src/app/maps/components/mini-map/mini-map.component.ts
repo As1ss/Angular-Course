dd7ea7e81ac66774211068c63570de95fb586746
As1ss <alexis.lopbri@educa.jcyl.es>
2024-05-03 09:14:39 +0200

    Proyecto MapsApp realizado feature de houses

--- 10-maps-app/src/app/maps/components/mini-map/mini-map.component.ts
+++ 10-maps-app/src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {Map, Marker} from "mapbox-gl";
+import { environment } from '../../../../environments/environment';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
+
+  @Input() coords?: [number,number];
+
+  @ViewChild("map")
+  public divMap?:ElementRef;
+
+  public map?:Map;
+
+
+
+
+  ngAfterViewInit(): void {
+
+    if(!this.coords) throw "Coords required";
+    if(!this.divMap?.nativeElement) throw "Map rendered error";
+
+    console.log(this.coords);
+
+    //crear mapa
+
+
+    this.map = new Map({
+      accessToken:
+        environment.mapbox_key,
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.coords, // starting position [lng, lat]
+      interactive: false,
+      zoom: 16, // starting zoom
+    });
+
+    //marker inicial
+
+    const marker = new Marker().setLngLat(this.map.getCenter()).addTo(this.map);
+    console.log(this.map.getCenter());
+  }
 
 }
