f04f26da9e7549c34b6c19485bd201511b5bedfe
As1ss <alexis.lopbri@educa.jcyl.es>
2024-05-06 14:19:22 +0200

    Proyecto Signals mutaciones en los usuarios

--- 11-directive-signals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 11-directive-signals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.css'
+  styleUrl: './properties-page.component.css',
 })
 export class PropertiesPageComponent {
+  public user = signal<User>({
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
 
+  public fullName = computed(
+    () => `${this.user().first_name} ${this.user().last_name}`
+  );
+
+  onFieldUpdated(field: keyof User, value: string) {
+
+
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value
+    // })
+
+    this.user.update( current => {
+
+      switch (field) {
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+
+      return current;
+    });
+  }
 }
